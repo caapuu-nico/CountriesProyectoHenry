654e69dd1be1db648357d03274b534384b2cab19
Nicolas Santiago Del Vecchio <nnicou.s@gmail.com>
2023-11-11 00:29:35 -0300

    ultimo commit antes de revisar y hacer deploy

--- countries pi/server/src/routes/index.js
+++ countries pi/server/src/routes/index.js
@@ -1,5 +1,10 @@
-const { Router } = require("express");
-
+const { Router } = require('express');
 const router = Router();
+const countriesRoute = require("./countriesRoute.js")
+const activitiesRoute = require("./activitiesRoute.js")
+
+router.use("/countries", countriesRoute)
+router.use("/activities",activitiesRoute)
+
 
 module.exports = router;
